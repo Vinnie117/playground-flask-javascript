4f1cafc82e9b78df53edc88b33b3e9de97662654
Vinnie117 <hhang@hotmail.de>
2022-11-18 20:32:52 +0100

    Flask tutorial MVP web app

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, request
-# from src.data import data
-
-
-
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-@app.route('/')
+with app.app_context():
+    db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/start', methods = ['POST', 'GET']) # methods that this route can accept
 def home():
     a = 3
     b = 4
@@ -14,13 +24,40 @@ def home():
 
     example_result = 'Python reads the table: '
 
-    return render_template('index.html', embed=example_embed, result = example_result) 
+    return render_template('index_old.html', embed=example_embed, result = example_result) 
+
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+# @app.route('/', methods = ['POST', 'GET']) # methods that this route can accept
+# def home():
+#     a = 3
+#     b = 4
+#     example_embed='This string is from python and the result is: {}'.format(a+b)
+
+#     if request.method == 'POST':
+#         c = request.form['content']
+#         example_result = 'Python reads the table: {}'.format(c)
+
+#     return render_template('index.html', embed=example_embed, result = example_result) 
 
 
 if __name__ == "__main__":
-
-    app.run(debug=True, host="0.0.0.0") # , host="0.0.0.0"
-
-    print('END')
-
-
+    app.run(debug=True)
