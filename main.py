4c0f5ce4c40631edae9f3f125d85c2c10854ff1d
Vinnie117 <hhang@hotmail.de>
2022-11-20 21:50:21 +0100

    flask fetch api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -64,7 +64,47 @@ def spreadsheet_1():
     return render_template('spreadsheet_1.html') 
 
 
+
+######## HOME ############
+@app.route('/fetch')
+def test_page():
+    example_embed='Sending data... [this is text from python]'
+    # look inside `templates` and serve `fetch.html`
+    return render_template('fetch.html', embed=example_embed)
+
+
+######## Example fetch ############
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'OK', 200
+    # GET request
+    else:
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
+
+
+######## Data fetch ############
+@app.route('/getdata/<transaction_id>/<second_arg>', methods=['GET','POST'])
+def datafind(transaction_id,second_arg):
+    # POST request
+    if request.method == 'POST':
+        print('Incoming..')
+        print(request.get_text())  # parse as text
+        return 'OK', 200
+    # GET request
+    else:
+        message = 't_in = %s ; result: %s ; opt_arg: %s'%(transaction_id, data[int(transaction_id)], second_arg)
+        return message #jsonify(message)  # serialize and use JSON headers
+
+
+
+
 if __name__ == "__main__":
+    data = list(range(1,300,3))
+    #print (data)
     app.run(debug=True)
 
 
